361c91b0eb3fc8f0194a59791bb89655fd718fd8
zakaria-69 <ladjrafi_zakaria@yahoo.com>
2022-08-24 01:32:42 +0200

    retrait code commenté

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -12,10 +12,6 @@ likes : {type :Number,default : 0 },
 dislikes : {type :Number,default : 0 },
 usersLiked : {type : [String]},
 usersDisliked : {type : [String]}
-/*usersLiked: { type: Array, default: [], required: true },
-usersDisliked: { type: Array, default: [], required: true },*/
-
-
 });
 
 module.exports = mongoose.model('Sauce' ,saucesSchema );
